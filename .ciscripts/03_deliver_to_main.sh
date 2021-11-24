733d0a7583423046435542613d4750c6357cfccb
Ferdinand Polycarpe ZAFITSARA <pzafitsara@bocasay.com>
2021-11-24 17:51:33 +0300

    Teste ci

--- .ciscripts/03_deliver_to_main.sh
+++ .ciscripts/03_deliver_to_main.sh
@@ -4,6 +4,6 @@ set -eux
 git config user.name ${GIT_USER}
 git config user.email ${GIT_EMAIL}
 git remote add ci-origin ${GIT_REPO}
-git checkout master
+git checkout main
 git merge develop
-git push ci-origin master
+git push ci-origin main
